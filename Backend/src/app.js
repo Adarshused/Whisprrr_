3d949342d245ea2dcfad1620b09cba827f99eed3
Adarshused <adarshmishr6@gmail.com>
2025-08-31 14:45:44 +0530

    Update CORS origin to use production URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://whisprrr-wo8g.vercel.app",
     methods:["GET","POST","OPTIONS","PUT","DELETE"],
     allowedHeaders: ["Content-Type","Authorization"],
     credentials: true
